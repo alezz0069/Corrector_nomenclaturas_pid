4f7735a05752ccbd13dd86f1407e8af87189cd5c
alezz0069 <69329366+alezz0069@users.noreply.github.com>
2023-11-29 19:38:13 +0100

    dataframe_tags.py

--- dataframe_tags.py
+++ dataframe_tags.py
@@ -1,63 +1,49 @@
 import pandas as pd
+from fuzzywuzzy import fuzz
 import re
-import difflib
 
-def calculate_refined_similarity_score(text, template):
-    # Splitting logic based on the specific template structure
-    text_components = text.split('-')
-    template_components = template.split('-')
-    total_similarity = 0
+# Function to create the four dataframes
+def create_tag_dataframes(df, patterns_templates):
+    # Function to convert text to template format
+    def convert_to_template_advanced(text, template):
+        converted_template = ''
+        digit_placeholder = '#'
+        letter_placeholder = 'X'
+        digit_count = template.count(digit_placeholder)
+        letter_count = template.count(letter_placeholder)
+        
+        for char in text:
+            if char.isdigit() and digit_count > 0:
+                converted_template += digit_placeholder
+                digit_count -= 1
+            elif char.isalpha() and letter_count > 0:
+                converted_template += letter_placeholder
+                letter_count -= 1
+            else:
+                converted_template += char
 
-    if len(text_components) == len(template_components):
-        for t_comp, tmpl_comp in zip(text_components, template_components):
-            # Using difflib to calculate the similarity for each component
-            similarity = difflib.SequenceMatcher(None, t_comp, tmpl_comp).ratio()
-            total_similarity += similarity
+        return converted_template
 
-        # Average the total similarity
-        average_similarity = (total_similarity / len(template_components)) * 100
-        return average_similarity
-    else:
-        return 0  # If the number of components doesn't match, return 0
+    # Function to calculate fuzzy score only if both exact matches are False
+    def calculate_fuzzy_score_advanced(row, pattern_key):
+        if not row['tuberia_score_100'] and not row['tag_score_100']:
+            pattern = patterns_templates[pattern_key]
+            text_template = convert_to_template_advanced(row['text_review'], pattern['template'])
+            return fuzz.token_sort_ratio(text_template, pattern['template'])
+        return 0
 
-def extract_and_difflib_match(text, compiled_pattern, template, min_threshold=10, max_threshold=99):
-    #"""Extract potential matches using regex and apply refined difflib matching."""
-    potential_matches = compiled_pattern.findall(text)
-    close_matches = []
-    for match in potential_matches:
-        similarity = calculate_refined_similarity_score(match, template)
-        if min_threshold <= similarity < max_threshold:  # Note: < max_threshold for non-exact matches
-            close_matches.append(match)
-    return close_matches
+    # Calculate perfect match scores
+    df['tuberia_score_100'] = df['text_review'].apply(lambda x: re.fullmatch(patterns_templates['TUBERIA_PATTERN']['regex_pattern'], x) is not None)
+    df['tag_score_100'] = df['text_review'].apply(lambda x: re.fullmatch(patterns_templates['TAG_PATTERN']['regex_pattern'], x) is not None)
 
-def categorize_text(df, compiled_tuberia_pattern, tuberia_template, compiled_tag_pattern, tag_template):
-    #"""Categorize text into different DataFrames based on regex patterns."""
-    tags_tuberias_correct_list = []
-    tags_equipos_correct_list = []
-    matched_tuberias_list = []
-    matched_equipos_list = []
+    # Apply the advanced fuzzy score calculation
+    df['tuberia_score_fuzzy'] = df.apply(lambda row: calculate_fuzzy_score_advanced(row, 'TUBERIA_PATTERN'), axis=1)
+    df['tag_score_fuzzy'] = df.apply(lambda row: calculate_fuzzy_score_advanced(row, 'TAG_PATTERN'), axis=1)
 
-    for index, row in df.iterrows():
-        text = row['text_review']
-        row_dict = row.to_dict()
-        if compiled_tuberia_pattern.fullmatch(text):
-            tags_tuberias_correct_list.append(row_dict)
-        elif compiled_tag_pattern.fullmatch(text):
-            tags_equipos_correct_list.append(row_dict)
-
-        tuberia_close_matches = extract_and_difflib_match(text, compiled_tuberia_pattern, tuberia_template)
-        if tuberia_close_matches:
-            for match in tuberia_close_matches:
-                matched_tuberias_list.append((row_dict, match))
-
-        equipos_close_matches = extract_and_difflib_match(text, compiled_tag_pattern, tag_template)
-        if equipos_close_matches:
-            for match in equipos_close_matches:
-                matched_equipos_list.append((row_dict, match))
-
-    TAGS_TUBERIAS_CORRECT = pd.DataFrame(tags_tuberias_correct_list)
-    TAGS_EQUIPOS_CORRECT = pd.DataFrame(tags_equipos_correct_list)
-    TAGS_TUBERIAS_CLOSE = pd.DataFrame([item[0] for item in matched_tuberias_list])
-    TAGS_EQUIPOS_CLOSE = pd.DataFrame([item[0] for item in matched_equipos_list])
+    # Filtering and creating the specified DataFrames
+    TAGS_TUBERIAS_CORRECT = df[df['tuberia_score_100']]
+    TAGS_EQUIPOS_CORRECT = df[df['tag_score_100']]
+    TAGS_TUBERIAS_CLOSE = df[(df['tuberia_score_fuzzy'] > 20) & (df['tuberia_score_fuzzy'] > df['tag_score_fuzzy'])]
+    TAGS_EQUIPOS_CLOSE = df[(df['tag_score_fuzzy'] > 20) & (df['tag_score_fuzzy'] > df['tuberia_score_fuzzy'])]
 
     return TAGS_TUBERIAS_CORRECT, TAGS_EQUIPOS_CORRECT, TAGS_TUBERIAS_CLOSE, TAGS_EQUIPOS_CLOSE
